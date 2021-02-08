4cb70c1cee615eb48191e36273f5b351fb2ab34d
Bruno Kelly <brunobuch00@gmail.com>
2021-02-08 20:55:23 -0300

    last version

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,4 @@
+if ( window.innerWidth <= 630 ) { 
 const carouselSlide = document.querySelector('.carousel-slide')
 const carouselDiv = document.querySelectorAll('.carousel-slide .projects-card')
 
@@ -10,7 +11,7 @@ const size = carouselDiv[0].clientWidth
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
 
 nextBtn.addEventListener('click', ()=>{
-    if( counter >=  carouselDiv.length - 2) return
+    if( counter >=  carouselDiv.length - 3) return
     carouselSlide.style.transition = 'transform 0.4s ease-in-out'
     counter++
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
@@ -18,9 +19,24 @@ nextBtn.addEventListener('click', ()=>{
 })
 
 prevBtn.addEventListener('click', ()=>{
-    if( counter <= -1) return
+    if( counter < 0) return
     carouselSlide.style.transition = 'transform 0.4s ease-in-out'
     counter--
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
 })
 
+carouselSlide.addEventListener('transitionend', () => {
+    if(carouselDiv[counter].id === 'lastSlide') 
+    {
+        carouselSlide.style.transtion = 'none'
+        counter = carouselDiv.length - 3
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+    }
+    if(carouselDiv[counter].id === 'firstSlide') 
+    {
+        carouselSlide.style.transtion = 'none'
+        counter = carouselDiv.length - 3
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+    }
+})
+}
